ab9190dfe5452210f0b98c7ef8be13cced45d580
Gopinath <25692465+devgopinath@users.noreply.github.com>
2022-07-16 11:51:20 +0530

    check sizeof

--- checker_test_data.c
+++ checker_test_data.c
@@ -1,65 +1,65 @@
 #include "checker.h"
 #include "checker_test.h"
 
-const BatteryAttributes BatteryInputs[] =
-{
-    // min limits
-    {
-        0.0,    //temperature
-        20.0,   //stateOfCharge
-        0.1,    //chargeRate
-        1  //expectedBattStatus
-    },
-    // max limits
-    {
-        45.0,    //temperature
-        80.0,   //stateOfCharge
-        0.8,    //chargeRate
-        1  //expectedBattStatus
-    },
-    // < min temp
-    {
-        -0.1,    //temperature
-        50.0,   //stateOfCharge
-        0.4,    //chargeRate
-        0  //expectedBattStatus
-    },
-    // > max temp
-    {
-        45.1,    //temperature
-        50.0,   //stateOfCharge
-        0.4,    //chargeRate
-        0  //expectedBattStatus
-    },
-    // < min soc
-    {
-        20.0,    //temperature
-        19.9,   //stateOfCharge
-        0.4,    //chargeRate
-        0  //expectedBattStatus
-    },
-    // > max soc
-    {
-        20.0,    //temperature
-        80.1,   //stateOfCharge
-        0.4,    //chargeRate
-        0  //expectedBattStatus
-    },
-    // < min chrate
-    {
-        20.0,    //temperature
-        50.0,   //stateOfCharge
-        0.0,    //chargeRate
-        0  //expectedBattStatus
-    },
-    // > max chrate
-    {
-        20.0,    //temperature
-        50.0,   //stateOfCharge
-        0.85,    //chargeRate
-        0  //expectedBattStatus
-    }
-};
+// const BatteryAttributes BatteryInputs[] =
+// {
+//     // min limits
+//     {
+//         0.0,    //temperature
+//         20.0,   //stateOfCharge
+//         0.1,    //chargeRate
+//         1  //expectedBattStatus
+//     },
+//     // max limits
+//     {
+//         45.0,    //temperature
+//         80.0,   //stateOfCharge
+//         0.8,    //chargeRate
+//         1  //expectedBattStatus
+//     },
+//     // < min temp
+//     {
+//         -0.1,    //temperature
+//         50.0,   //stateOfCharge
+//         0.4,    //chargeRate
+//         0  //expectedBattStatus
+//     },
+//     // > max temp
+//     {
+//         45.1,    //temperature
+//         50.0,   //stateOfCharge
+//         0.4,    //chargeRate
+//         0  //expectedBattStatus
+//     },
+//     // < min soc
+//     {
+//         20.0,    //temperature
+//         19.9,   //stateOfCharge
+//         0.4,    //chargeRate
+//         0  //expectedBattStatus
+//     },
+//     // > max soc
+//     {
+//         20.0,    //temperature
+//         80.1,   //stateOfCharge
+//         0.4,    //chargeRate
+//         0  //expectedBattStatus
+//     },
+//     // < min chrate
+//     {
+//         20.0,    //temperature
+//         50.0,   //stateOfCharge
+//         0.0,    //chargeRate
+//         0  //expectedBattStatus
+//     },
+//     // > max chrate
+//     {
+//         20.0,    //temperature
+//         50.0,   //stateOfCharge
+//         0.85,    //chargeRate
+//         0  //expectedBattStatus
+//     }
+// };
 
 #define TEST_PARAMETER_MAX 80.0
 #define TEST_PARAMETER_MIN 20.0
